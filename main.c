00860dbf1098ebf212158e613696de5846474552
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2024-09-25 00:01:05 +0000

    menu criado

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+#define MAXN 11 // Polinômios de grau 0 até 10
+
 int main(){
-    printf("teste\n");
+    int polinomio[MAXN];
+    int option;
+
+    do{
+        printf("---CALCULADORA DE POLINOMIOS---\n\n");
+        printf("1. Calcular valor de polinomio\n");
+        printf("2. Calcular soma de polinomios\n");
+        printf("3. Calcular produto de polinomios\n");
+        printf("4. Sair\n");
+        printf("\nOpção ---> ");
+        
+        scanf("%d", &option);
+
+    
+    } while (option != 4);
+
 }
