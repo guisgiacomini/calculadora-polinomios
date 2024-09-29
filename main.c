65a6424b368de3e7737af3ccad846e939207ef32
guisgiacomini <157508090+guisgiacomini@users.noreply.github.com>
2024-09-29 18:14:34 +0000

    calcula_valor

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 #define MAXN 11 // Polinômios de grau 0 até 10
 
-void mostra_polinomio(int polinomio[], int grau);
-int calcula_valor(int polinomio[], int grau, int x);
+void mostra_polinomio(float polinomio[], int grau);
+float calcula_valor(float polinomio[], int grau, int x);
 
 int main(){
 
-    int polinomio1[MAXN], polinomio2[MAXN], polinomio3[MAXN], option, grau, x;
+    float polinomio1[MAXN], polinomio2[MAXN], polinomio3[MAXN];
+    int option, grau, x;
     
     do{
         
@@ -36,20 +38,18 @@ int main(){
 
             // leitura do polinômio
             printf("Insira os coeficientes do polinomio de forma crescente\n---> ");
-            for (int i = 0; i < grau; i++)
+            for (int i = 0; i < grau + 1; i++)
             {
-                scanf("%d", &polinomio1[i]);
+                scanf("%f", &polinomio1[i]);
             }
 
             mostra_polinomio(polinomio1, grau);
-            printf("Insira o valor da incognita X\n---> ");
+            printf("\nInsira o valor da incognita X\n---> ");
             scanf("%d", &x);
 
             
-            int resultado = calcula_valor(polinomio1, grau, x);
-
-
-
+            float resultado = calcula_valor(polinomio1, grau, x);
+            printf("P(%d) = %g\n", x, resultado);
         }
 
     
@@ -57,22 +57,32 @@ int main(){
 
 }
 
-void mostra_polinomio(int polinomio[], int grau){
+void mostra_polinomio(float polinomio[], int grau){
     
-    for(int i = grau - 1; i >= 0; i--)
+    printf("P(x) = ");
+    for(int i = grau; i >= 0; i--)
     {
         if(i == 0)
         {
-            printf("%d\n", polinomio[i]);
+            printf("%g\n", polinomio[i]);
         }
         else if (polinomio[i] != 0)
         {
-            printf("%dx^%d + ", polinomio[i], i);
+
+            printf("%gx^%d + ", polinomio[i], i);
         }
     }
 
 }
 
-int calcula_valor(int polinomio[], int grau, int x){
+float calcula_valor(float polinomio[], int grau, int x){
 
+    float resultado = 0;
+
+    for(int i = 0; i <= grau; i++)
+    {
+        resultado += polinomio[i] * pow(x, i);
+    }
+
+    return resultado;
 }
